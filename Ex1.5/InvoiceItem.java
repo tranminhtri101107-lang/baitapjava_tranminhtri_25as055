f409938d88abefe4ac8d190e11d897216b101151
MinhTri <tranminhtri101107@gmail.com>
2025-10-15 22:49:54 +0700

    baitapJava

---
+++ Ex1.5/InvoiceItem.java
@@ -0,0 +1,40 @@
+public class InvoiceItem {
+    private String id;
+    private String desc;
+    private int qty;
+    private double unitPrice;
+
+    public InvoiceItem(String id, String desc, int qty, double unitPrice) {
+        this.id = id;
+        this.desc = desc;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getDesc() {
+        return desc;
+    }
+    public int getQty() {
+        return qty;
+    }
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+    public double getTotalPrice() {
+        return unitPrice * qty;
+    }
+    public String toString() {
+        return id + "\t" + desc + "\t" + qty + "\t" + unitPrice;
+    }
+    public double getTotal() {
+        return qty * unitPrice;
+    }
+}
